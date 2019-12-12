efed68a904aa26f9ba51d9ef41e19c8e7d8352e1
Karl Nixon Domingo <50222686+karlnixondomingo@users.noreply.github.com>
2019-12-12 21:23:00 +0800

    Update signup.js

--- cypress/integration/signup.js
+++ cypress/integration/signup.js
@@ -1,16 +1,30 @@
 describe("Examination - Senior QA", () => {
 	it("Test Case #1 - That an invalid email address is rejected.", () => {
 		cy.visit("https://signup.velpic.com");
+	
+		// type an invalid email
 		cy.get('[name=email]').type('invalid email')
+		
+		// click Get Started button
 		cy.contains('Get Started').click()
-		cy.get('[class=form-control-feedback]').contains('Please check that your email address is entered correctly').should('exist')
+		
+		// assert email textbox still exist
+		cy.get('[name=email]').should('exist')
 
 	});
 	it("Test Case #2 - That a valid email address is accepted.", () => {
 		cy.visit("https://signup.velpic.com");
+		
+		// type a valid email
 		cy.get('[name=email]').type('sample@email.com')
+		
+		// click Get Started button
 		cy.contains('Get Started').click()
+		
+		// assert email textbox should not exist
+		cy.get('[name=email]').should('not.exist')
+		
+		// assert name textbox exists
 		cy.get('[name=name]').should('exist')
-		cy.get('[name=phonenumber]').should('exist')
 	});
 });
