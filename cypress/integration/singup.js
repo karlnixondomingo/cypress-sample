333a48cf9ffd4e9c6c782b4ba042d96a6f43c09a
Karl Nixon Domingo <50222686+karlnixondomingo@users.noreply.github.com>
2019-12-12 20:48:32 +0800

    "Added examination scenarios to signup.js"

--- cypress/integration/singup.js
+++ cypress/integration/singup.js
@@ -1,6 +1,16 @@
-it("email validation works during signup", () => {
-	cy.visit("https://signup.velpic.com");
+describe("Examination - Senior QA", () => {
+	it("Test Case #1 - That an invalid email address is rejected.", () => {
+		cy.visit("https://signup.velpic.com");
+		cy.get('[name=email]').type('invalid email')
+		cy.contains('Get Started').click()
+		cy.get('[class=form-control-feedback]').contains('Please check that your email address is entered correctly').should('exist')
 
-
-
+	});
+	it("Test Case #2 - That a valid email address is accepted.", () => {
+		cy.visit("https://signup.velpic.com");
+		cy.get('[name=email]').type('sample@email.com')
+		cy.contains('Get Started').click()
+		cy.get('[name=name]').should('exist')
+		cy.get('[name=phonenumber]').should('exist')
+	});
 });
